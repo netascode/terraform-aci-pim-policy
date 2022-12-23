d1da02101a62dae8ca507eefc63639c15fdfb8cf
jgomezve <jgomezve@cisco.com>
2022-12-23 09:59:55 -0500

    Initial commit. Execution plan

--- README.md
+++ README.md
@@ -38,20 +38,36 @@ module "aci_scaffolding" {
 
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
-| <a name="input_name"></a> [name](#input\_name) | Tenant name. | `string` | n/a | yes |
-| <a name="input_alias"></a> [alias](#input\_alias) | Tenant alias. | `string` | `""` | no |
-| <a name="input_description"></a> [description](#input\_description) | Tenant description. | `string` | `""` | no |
+| <a name="input_name"></a> [name](#input\_name) | PIM Policy name. | `string` | n/a | yes |
+| <a name="input_tenant"></a> [tenant](#input\_tenant) | PIM Policy's Tenant name. | `string` | n/a | yes |
+| <a name="input_auth_key"></a> [auth\_key](#input\_auth\_key) | PIM Policy Authorization Key. | `string` | `""` | no |
+| <a name="input_auth_type"></a> [auth\_type](#input\_auth\_type) | PIM Policy Authorization Type. Valid values are: `none` or `ah-md5`. | `string` | `"none"` | no |
+| <a name="input_mcast_dom_boundary"></a> [mcast\_dom\_boundary](#input\_mcast\_dom\_boundary) | PIM Policy Multicast Domain Boundary Flag. | `bool` | `false` | no |
+| <a name="input_passive"></a> [passive](#input\_passive) | PIM Policy Multicast Strict RFC Compliant Flag. | `bool` | `false` | no |
+| <a name="input_designated_router_delay"></a> [designated\_router\_delay](#input\_designated\_router\_delay) | PIM Policy Designated Router Delay (seconds). | `number` | `3` | no |
+| <a name="input_designated_router_priority"></a> [designated\_router\_priority](#input\_designated\_router\_priority) | PIM Policy Multicast Designated Router Priority. | `number` | `1` | no |
+| <a name="input_hello_interval"></a> [hello\_interval](#input\_hello\_interval) | PIM Policy Multicast Hello Interval (milliseconds). | `number` | `30000` | no |
+| <a name="input_join_prune_interval"></a> [join\_prune\_interval](#input\_join\_prune\_interval) | PIM Policy Join Prune Interval (seconds). | `number` | `60` | no |
+| <a name="input_neighbor_filter_policy"></a> [neighbor\_filter\_policy](#input\_neighbor\_filter\_policy) | PIM Policy Interface-Level Neighbor Filter Policy. | `string` | `""` | no |
+| <a name="input_join_prune_filter_policy_out"></a> [join\_prune\_filter\_policy\_out](#input\_join\_prune\_filter\_policy\_out) | PIM Policy Interface-Level Outbound Join-Prune Filter Policy. | `string` | `""` | no |
+| <a name="input_join_prune_filter_policy_in"></a> [join\_prune\_filter\_policy\_in](#input\_join\_prune\_filter\_policy\_in) | PIM Policy Interface-Level Inbound Join-Prune Filter Policy. | `string` | `""` | no |
 
 ## Outputs
 
 | Name | Description |
 |------|-------------|
-| <a name="output_dn"></a> [dn](#output\_dn) | Distinguished name of `fvTenant` object. |
-| <a name="output_name"></a> [name](#output\_name) | Tenant name. |
+| <a name="output_dn"></a> [dn](#output\_dn) | Distinguished name of `pimIfPol` object. |
+| <a name="output_name"></a> [name](#output\_name) | PIM Policy name. |
 
 ## Resources
 
 | Name | Type |
 |------|------|
-| [aci_rest_managed.fvTenant](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/resources/rest_managed) | resource |
+| [aci_rest_managed.pimIfPol](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/resources/rest_managed) | resource |
+| [aci_rest_managed.pimJPInbFilterPol](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/resources/rest_managed) | resource |
+| [aci_rest_managed.pimJPOutbFilterPol](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/resources/rest_managed) | resource |
+| [aci_rest_managed.pimNbrFilterPol](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/resources/rest_managed) | resource |
+| [aci_rest_managed.rtdmcRsFilterToRtMapPol_join_prune_filter_in](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/resources/rest_managed) | resource |
+| [aci_rest_managed.rtdmcRsFilterToRtMapPol_join_prune_filter_out](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/resources/rest_managed) | resource |
+| [aci_rest_managed.rtdmcRsFilterToRtMapPol_neighbor_filter](https://registry.terraform.io/providers/CiscoDevNet/aci/latest/docs/resources/rest_managed) | resource |
 <!-- END_TF_DOCS -->
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  description = "Tenant name."
+  description = "PIM Policy name."
   type        = string
 
   validation {
@@ -8,24 +8,140 @@ variable "name" {
   }
 }
 
-variable "alias" {
-  description = "Tenant alias."
+variable "tenant" {
+  description = "PIM Policy's Tenant name."
   type        = string
-  default     = ""
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.alias))
+    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.tenant))
     error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
   }
 }
 
-variable "description" {
-  description = "Tenant description."
+variable "auth_key" {
+  description = "PIM Policy Authorization Key."
   type        = string
   default     = ""
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9\\!#$%()*,-./:;@ _{|}~?&+]{0,128}$", var.description))
-    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `\\`, `!`, `#`, `$`, `%`, `(`, `)`, `*`, `,`, `-`, `.`, `/`, `:`, `;`, `@`, ` `, `_`, `{`, `|`, }`, `~`, `?`, `&`, `+`. Maximum characters: 128."
+    condition     = can(regex("^[a-zA-Z0-9_.-]{0,130}$", var.auth_key))
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 130."
+  }
+}
+
+variable "auth_type" {
+  description = "PIM Policy Authorization Type. Valid values are: `none` or `ah-md5`. Default value is `none`."
+  type        = string
+  default     = "none"
+
+  validation {
+    condition     = contains(["none", "ah-md5"], var.auth_type)
+    error_message = "Valid values are: `none` or `ah-md5`."
+  }
+}
+
+variable "auth_type" {
+  description = "PIM Policy Authorization Type. Valid values are: `none` or `ah-md5`."
+  type        = string
+  default     = "none"
+
+  validation {
+    condition     = contains(["none", "ah-md5"], var.auth_type)
+    error_message = "Valid values are: `none` or `ah-md5`."
+  }
+}
+
+variable "mcast_dom_boundary" {
+  description = "PIM Policy Multicast Domain Boundary Flag."
+  type        = bool
+  default     = false
+}
+
+variable "passive" {
+  description = "PIM Policy Multicast Passive Flag."
+  type        = bool
+  default     = false
+}
+
+variable "passive" {
+  description = "PIM Policy Multicast Strict RFC Compliant Flag."
+  type        = bool
+  default     = false
+}
+
+variable "designated_router_delay" {
+  description = "PIM Policy Designated Router Delay (seconds)."
+  type        = number
+  default     = 3
+
+  validation {
+    condition     = var.designated_router_delay >= 1 && var.designated_router_delay <= 65535
+    error_message = "Allowed values `1`-`65535`."
+  }
+}
+
+variable "designated_router_priority" {
+  description = "PIM Policy Multicast Designated Router Priority."
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = var.designated_router_priority >= 1 && var.designated_router_priority <= 4294967295
+    error_message = "Allowed values `1`-`4294967295`."
+  }
+}
+
+variable "hello_interval" {
+  description = "PIM Policy Multicast Hello Interval (milliseconds)."
+  type        = number
+  default     = 30000
+
+  validation {
+    condition     = var.hello_interval >= 1 && var.hello_interval <= 18724286
+    error_message = "Allowed values `1`-`18724286`."
+  }
+}
+
+variable "join_prune_interval" {
+  description = "PIM Policy Join Prune Interval (seconds)."
+  type        = number
+  default     = 60
+
+  validation {
+    condition     = var.join_prune_interval >= 60 && var.join_prune_interval <= 65520
+    error_message = "Allowed values `60`-`65520`."
+  }
+}
+
+variable "neighbor_filter_policy" {
+  description = "PIM Policy Interface-Level Neighbor Filter Policy."
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.neighbor_filter_policy))
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+  }
+}
+
+variable "join_prune_filter_policy_out" {
+  description = "PIM Policy Interface-Level Outbound Join-Prune Filter Policy."
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.join_prune_filter_policy_out))
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+  }
+}
+
+variable "join_prune_filter_policy_in" {
+  description = "PIM Policy Interface-Level Inbound Join-Prune Filter Policy."
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.join_prune_filter_policy_in))
+    error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
   }
 }
